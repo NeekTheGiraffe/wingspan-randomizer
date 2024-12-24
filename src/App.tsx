0bee396114508b598342c5b478774c2279fb0bf3
Nick Pavlosky <npavlosky@gmail.com>
2024-12-23 17:35:50 -0800

    Add basic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { generateDuetMap, toString, neighbors } from './core/DuetMap'
+import { generateDuetMap, toString, neighbors, DuetMap, Habitat } from './core/DuetMap'
 import { MersenneTwister19937 as mt } from 'random-js'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [map, setMap] = useState(generateDuetMap(mt.seed(0)));
+function dotRepresentation(map: Habitat[]) {
+  const helperClasses: Record<number, string> = {
+    0: "southeast-6",
+    1: "southeast-6 southwest-3",
+    2: "southeast-6 southwest-5",
+    3: "southeast-6 southwest-6",
+    4: "southeast-4 southwest-6",
+    5: "southeast-2 southwest-6",
+    11: "southwest-5",
+    12: "southeast-4",
+    23: "southwest-3",
+    24: "southeast-2",
+  }
 
-  // for (let i = 0; i < 36; i++) {
-  //   console.log(i, neighbors(i));
-  // }
+  return <div className="duet-map">
+    {[...Array(6).keys()].map(row => 
+    <div key={row} className={`row ${row % 2 === 0 ? 'even' : 'odd'}`}>
+      {map.slice(6 * row, 6 * (row + 1)).map((habitat, col) =>
+        <div key={6 * row + col} className={`dot-container ${helperClasses[6 * row + col] ?? ''}`}><span className={`dot ${habitat} `} /></div>)}
+    </div>
+  )}
+  </div>;
+}
+
+const initialMap = generateDuetMap(mt.autoSeed());
+
+function App() {
+  const [map, setMap] = useState(initialMap);
 
   return (
     <>
-      <code>{toString(map)}</code>
+      {/* <code>{toString(map)}</code> */}
+      <button onClick={() => setMap(generateDuetMap(mt.autoSeed()))}>Regenerate</button><br/>
+      {dotRepresentation(map.habitats)}
+      
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
